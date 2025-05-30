6c8e6341228ff9e1967104f0422e6b1d834d7098
AfshanAlamEngg <afshanalamengg@gmail.com>
2025-05-31 01:06:59 +0530

    Added: Logging in project and Connection to H2

--- subscription/src/main/java/com/qrencia/subscription/SubscriptionApplication.java
+++ subscription/src/main/java/com/qrencia/subscription/SubscriptionApplication.java
@@ -1,13 +1,17 @@
 package com.qrencia.subscription;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SubscriptionApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SubscriptionApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SubscriptionApplication.class, args);
+		logger.info("Application started");
 	}
 
 }
